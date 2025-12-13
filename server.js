7d65b72b43fa8ba0afcfcab5b1d5ae1e8921fe43
Anngeel <134184708+Anngeel@users.noreply.github.com>
2025-12-12 22:26:34 -0600

    Actualizacion de rutas para el administrador
    Ahora el administrador puede inhabilitar usuarios y platillos

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const { v4: uuidv4 } = require('uuid');
 const multer = require("multer");
 const app = express();
 
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
